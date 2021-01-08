e5ac6fa50fba78d49bca6724eebe579c73fbaf3b
Sangram K Sahu <sangram@lifebit.ai>
2021-01-08 20:33:37 +0530

    CUBE manifest parser (#1)
    * wip
    
    * final fixes
    
    * minor fixes
    
    * improvements
    
    * typo fix
    
    * Minor typo fix
    
    Co-authored-by: cgpu <38183826+cgpu@users.noreply.github.com>

---
+++ bin/parse_cube_manifest.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# A simple json to csv parser, with focus on extracting perticular columns in CUBE data
+# depends upon: csvkit
+# pip install csvkit 
+
+print_usage() {
+  printf "\n"
+  printf "Usage: parse_cube_manifest.sh -m manifest.json \n\n"
+  printf "   -m:    A input manifest file (ex - manifest.json) [required] \n"
+  printf "   -o:    A output file name (ex - manifest.csv) [optional] \n"
+  printf "          If no output given it will be same as input \n"
+  printf "   -h:    This help menu \n\n"
+}
+
+while getopts 'm:o:h' flag
+do
+    case "${flag}" in
+      m) manifest_json="${OPTARG}" ;;
+      o) output_csv="${OPTARG}" ;;
+      h)
+        print_usage
+        exit 1
+        ;;
+      *)
+        print_usage
+        exit 1
+        ;;
+    esac
+done
+
+# check required params
+if [ -z "$manifest_json" ]; then
+  echo "A input is required in json format, Check help -h"
+  exit 1 ; 
+fi
+
+# check if output file name is given or else based on input file
+if [ -z "$output_csv" ]; then
+  output_csv=$(basename "$manifest_json" | sed s/.json/.csv/)
+fi
+
+# convert from json to desired csv
+in2csv "$manifest_json" > tmp_manifest.csv
+awk -F, '{ print $1","$4$3 }' tmp_manifest.csv > "$output_csv"
+rm tmp_manifest.csv
